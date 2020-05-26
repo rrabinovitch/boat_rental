69ab2eea3a16a03e1105ac0cfadcb9dbf69c34e8
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 00:08:46 -0600

    Complete basics of boat class and test

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -1,3 +1,30 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/boat'
+
+class BoatNameTest < Minitest::Test
+  def setup
+    @kayak = Boat.new(:kayak, 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Boat, @kayak
+  end
+
+  def test_it_has_type_and_price
+    assert_equal :kayak, @kayak.type
+    assert_equal 20, @kayak.price_per_hour
+  end
+
+  def test_it_starts_with_zero_hours_rented
+    assert_equal 0, @kayak.hours_rented
+  end
+
+  def test_hours_can_be_added
+    @kayak.add_hour
+    @kayak.add_hour
+    @kayak.add_hour
+
+    assert_equal 3, @kayak.hours_rented
+  end
+end
