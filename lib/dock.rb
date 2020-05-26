5f0ac7e0d05b787f1de107fa221c4dfb7e230ab3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 17:24:03 -0600

    Add charge method, passing for charges w hours fewer than max rental time

--- lib/dock.rb
+++ lib/dock.rb
@@ -10,4 +10,18 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    charge = {}
+
+    if boat.hours_rented <= @max_rental_time
+      charge[:card_number] = @rental_log[boat].credit_card_number
+      charge[:amount] = (boat.price_per_hour * boat.hours_rented)
+      charge
+    else
+      ###
+      require "pry"; binding.pry
+    end
+
+  end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -41,21 +41,41 @@ class DockTest < Minitest::Test
     @dock.rent(@kayak_2, @patrick)
     @dock.rent(@sup_1, @eugene)
 
-    expected_rental_log = {@kayak_1 => @patrick,
-                          @kayak_2 => @patrick,
-                          @sup_1 => @eugene}
+    rental_log = {@kayak_1 => @patrick,
+                    @kayak_2 => @patrick,
+                    @sup_1 => @eugene}
 
-    assert_equal expected_rental_log, @dock.rental_log
+    assert_equal rental_log, @dock.rental_log
   end
 
   def test_it_can_charge_for_rentals
+    # three boats are rented to three respective renters
     @dock.rent(@kayak_1, @patrick)
     @dock.rent(@kayak_2, @patrick)
     @dock.rent(@sup_1, @eugene)
 
-    expected_rental_log = {@kayak_1 => @patrick,
-                          @kayak_2 => @patrick,
-                          @sup_1 => @eugene}
+    # two hours of rental time are added to one of patrick's rentals
+    @kayak_1.add_hour
+    @kayak_1.add_hour
 
+    kayak_1_charge = {:card_number => "4242424242424242",
+                      :amount => 40}
+
+    # uses patrick's cc number
+    # calculates how much he should be charged for boat rental,
+      # up to dock's max rental time
+
+    assert_equal kayak_1_charge, @dock.charge(@kayak_1)
+
+    # @sup_1.add_hour
+    # @sup_1.add_hour
+    # @sup_1.add_hour
+    # @sup_1.add_hour
+    # @sup_1.add_hour
+    #
+    # sup_1_charge = {:card_number => "1313131313131313",
+    #                   :amount => 45}
+    #
+    # assert_equal sup_1_charge, @dock.charge(@sup_1)
   end
 end
